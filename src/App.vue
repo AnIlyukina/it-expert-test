0f8937d5b03c760cb2275fca9120f845d89ca43a
Ilyukina <anyailyukina@yandex.ru>
2022-10-08 09:09:41 +0800

    Создание проекта, первоначальная структура папок

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <p>Основоной компонент</p>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  
+  }
+}
+</script>
+
+<style>
+@import './assets/app.scss';
+</style>
