43e20a3eae86e6246b9d12ff469c45b49846b330
Ilyukina <anyailyukina@yandex.ru>
2022-10-08 16:16:39 +0800

    Реализовала группировку и сортировку массива users с помощью computed свойства, вывела их на страницу

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
   <button @click="getDate">Получить данные</button>
+  <ul>
+    <li v-for="user in sortedUsersList" :key="user.userId">
+      Пользователь {{user.userId}} 
+      <span> {{ user.countCompleted}} / {{user.todos.length - user.countCompleted }} </span>
+    </li>
+  </ul>
 </template>
 
 <script>
@@ -11,13 +17,57 @@ export default {
   components: {
   
   },
+  data () {
+    return {
+      usersList: []
+    }
+  },
+  computed: {
+    sortedUsersList () {
+      
+      if (this.usersList.length) {
+        const grouppedTodos = this.usersList.reduce((acc, cur) => {
+          acc[cur.userId] = acc[cur.userId] || {
+            userId: cur.userId,
+            todos: [],
+            countCompleted: 0
+          }
+          acc[cur.userId].todos.push({
+            id: cur.id,
+            title: cur.title,
+            completed: cur.completed
+          })
+          return acc
+        }, {})
+        const sortedbyUserId = Object.values(grouppedTodos)
+
+        sortedbyUserId.forEach((user) => {
+          user.todos.forEach(todo => {
+            if (todo.completed) {
+              user.countCompleted += 1
+            }
+          })
+        })
+
+        const sortedByСountCompleted = sortedbyUserId.sort((a, b) => {
+          if (a.countCompleted > b.countCompleted) {
+            return -1;
+          }
+          if (a.countCompleted < b.countCompleted) {
+            return 1;
+          }
+          return 0;
+        })
+        
+        return sortedByСountCompleted
+      } else {
+        return []
+      }
+    }
+  },
   methods: {
-    getDate () {
-      console.log('отправляем данные')
-      getUsersDate()
-      .then(response => {
-        console.log(response)
-      })
+    async getDate() {
+      this.usersList = await getUsersDate();
     }
   }
 }
